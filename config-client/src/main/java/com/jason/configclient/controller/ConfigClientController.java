61d3ebd4f93bb2d87cae52c87f2aa03b361c53e7
八两 <332857067@qq.com>
2020-04-08 11:38:02 +0800

    SpringCloudConfig学习

---
+++ config-client/src/main/java/com/jason/configclient/controller/ConfigClientController.java
@@ -0,0 +1,21 @@
+package com.jason.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jason
+ * @date 2020/4/7 15:51
+ * @description
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/profile")
+    public String hello(){
+        return profile;
+    }
+}
